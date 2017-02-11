4f7125171878ceff8164b586b2105229e12a531f
Dennis Zinzi <dennis@spontly.com>
2017-02-11 00:24:33 +0000

    Added Unreal files

---
+++ EscapeRoomGame/Intermediate/Build/Mac/UE4Editor/Development/EscapeRoomGame/EscapeRoomGame.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_ESCAPEROOMGAME_H__
+#define __AUTO_ESCAPEROOMGAME_H__
+//Last Write: 11/02/2017 00:19:43 +00:00
+#include "/Volumes/DennisZinziEXT/Development/UnrealEngine/EscapeRoom/EscapeRoomGame/Source/EscapeRoomGame/EscapeRoomGame.h"
+#endif//__AUTO_ESCAPEROOMGAME_H__
---
+++ EscapeRoomGame/Source/EscapeRoomGame/EscapeRoomGame.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EscapeRoomGame.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EscapeRoomGame, "EscapeRoomGame" );
